5ead0977b4f77a283d4d97698343867d90889edd
ahmed.es <essakouri.ahmed.97@gmail.com>
2025-08-11 21:13:39 +0200

    Feature & Fix: Fixing path problem with sys path append tech + adding some tets func

--- mlops-cicd/tests/pipelines/feature_engineering/test_pipeline.py
+++ mlops-cicd/tests/pipelines/feature_engineering/test_pipeline.py
@@ -10,8 +10,12 @@ https://docs.pytest.org/en/latest/getting-started.html
 
 import pandas as pd
 import pytest
+import sys
+import os
 
-from src.mlops_cicd.pipelines.feature_engineering.nodes import process_passenger_id
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
+
+from src.mlops_cicd.pipelines.feature_engineering.nodes import process_passenger_id, process_cabin, process_name
 
 @pytest.mark.parametrize(
         "df_input, df_output",
@@ -30,4 +34,44 @@ from src.mlops_cicd.pipelines.feature_engineering.nodes import process_passenger
 )
 def test_process_passenger_id(df_input, df_output):
     result = process_passenger_id(df_input)
+    pd.testing.assert_frame_equal(result, df_output)
+
+@pytest.mark.parametrize(
+    "df_input, df_output",
+    [
+        (
+            pd.DataFrame({
+                "Cabin" : ["C/123/B", "A/123/C", "S/340/D"]
+            }),
+            pd.DataFrame({
+                "Cabin" : ["C/123/B", "A/123/C", "S/340/D"],
+                "Cabin1" : ["C", "A", "S"],
+                "Cabin2" : ["123", "123", "340"],
+                "Cabin3" : ["B", "C", "D"]
+            })
+        )
+    ]
+)
+def test_process_cabin(df_input, df_output):
+    result = process_cabin(df_input)
+    pd.testing.assert_frame_equal(result, df_output)
+
+
+@pytest.mark.parametrize(
+    "df_input, df_output",
+    [
+        (
+            pd.DataFrame({
+                "Name" : ["Ahmed Es", "Tom Ch", "Mahdi Ba"],
+            }),
+            pd.DataFrame({
+                "Name" : ["Ahmed Es", "Tom Ch", "Mahdi Ba"],
+                "first_name" : ["Ahmed", "Tom", "Mahdi"],
+                "last_name" : ["Es", "Ch", "Ba"],
+            })
+        )
+    ]
+)
+def test_process_name(df_input, df_output):
+    result = process_name(df_input)
     pd.testing.assert_frame_equal(result, df_output)
